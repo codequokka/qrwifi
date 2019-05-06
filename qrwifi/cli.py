0b8cc2f18e958cd64ac320be1a68a67c96aa7bdc
Nobuchika Tanaka <codequokka@gmail.com>
2019-05-07 08:02:39 +0900

    Implement git like subcommand

--- qrwifi/cli.py
+++ qrwifi/cli.py
@@ -4,33 +4,31 @@ from .functions import wifi_qr
 
 
 @click.group()
+def main():
+    pass
+
+
+@main.command()
 @click.option("--ssid", help="WiFi network name.")
 @click.option("--security", type=click.Choice(["WEP", "WPA", ""]))
 @click.option("--password", help="WiFi password.")
-@click.pass_context
-def main(ctx, ssid: str, security: str = "", password: str = ""):
+def terminal(ssid, security, password):
     qr = wifi_qr(ssid=ssid, security=security, password=password)
-    ctx.obj["qr"] = qr
-    ctx.obj["ssid"] = ssid
-    ctx.obj["security"] = security
-    ctx.obj["password"] = password
+    print(qr.terminal())
 
 
 @main.command()
-@click.pass_context
-def terminal(ctx):
-    print(ctx.obj["qr"].terminal())
-
-
-@main.command()
-@click.option("--filename", help="full path to the png file")
-@click.pass_context
-def png(ctx, filename, scale: int = 10):
-    ctx.obj["qr"].png(filename, scale)
+@click.option("--ssid", help="WiFi network name.")
+@click.option("--security", type=click.Choice(["WEP", "WPA", ""]))
+@click.option("--password", help="WiFi password.")
+@click.option("--filename", help="full path to the png file", required=True)
+def png(ssid, security, password, filename, scale=10):
+    qr = wifi_qr(ssid=ssid, security=security, password=password)
+    qr.png(filename, scale)
 
 
 def start():
-    main(obj={})
+    main()
 
 
 if __name__ == "__main__":
